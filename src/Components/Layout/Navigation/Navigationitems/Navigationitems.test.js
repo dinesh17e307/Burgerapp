55fa91bf98815f998d0fd5f96e431093c875e7dc
DINESH S <dinisellappan@gmail.com>
2021-03-12 13:28:56 +0530

    testing the navigationitems logout

--- src/Components/Layout/Navigation/Navigationitems/Navigationitems.test.js
+++ src/Components/Layout/Navigation/Navigationitems/Navigationitems.test.js
@@ -5,8 +5,23 @@ import NavigationItem from "../Navigationitem/Navigationitem";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 describe("<Navigationitems/>", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Navigationitems />);
+  });
   it("it should render two link navigations while not authenticating", () => {
-    const wrapper = shallow(<Navigationitems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  it("it should render two link navigations while  authenticating", () => {
+    // wrapper = shallow(<Navigationitems isauth />);
+    wrapper.setProps({ isauth: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+  it("it should render two link navigations while logout authenticating", () => {
+    // wrapper = shallow(<Navigationitems isauth />);
+    wrapper.setProps({ isauth: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">logout</NavigationItem>)
+    ).toEqual(true);
   });
 });
