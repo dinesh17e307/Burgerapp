1ac8e446391b7f073fba11ab9c0045fb1966babf
DINESH S <dinisellappan@gmail.com>
2021-04-07 16:55:45 +0530

    lazy loading react

--- src/App.js
+++ src/App.js
@@ -3,44 +3,53 @@ import Layout from "./Components/Layout/Layout";
 import Burgerbuilder from "./Container/BurgerBuilder/Burgerbuilder";
 import Http from "./Container/BurgerBuilder/HTTP/Http";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
-import Checkout from "./Container/Checkout/Checkout";
-import Orders from "./Components/Orders/Orders";
+
 import Auth from "./Container/Auth/Auth";
 import Logout from "./Container/Auth/logout/Logout";
 import { connect } from "react-redux";
 import * as actions from "./Store/actions/index";
-import React, { Component } from "react";
-class App extends Component {
-  componentDidMount() {
-    this.props.onauthcheck();
-  }
-  render() {
-    let route = (
+import React, { useEffect, Suspense } from "react";
+const Order = React.lazy(() => {
+  return import("./Components/Orders/Orders");
+});
+const Checkout = React.lazy(() => {
+  return import("./Container/Checkout/Checkout");
+});
+const Auths = React.lazy(() => {
+  return import("./Container/Auth/Auth");
+});
+const App = (props) => {
+  useEffect(() => {
+    props.onauthcheck();
+  }, []);
+  let route = (
+    <Switch>
+      <Route exact path="/" component={Burgerbuilder} />
+      <Route exact path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (props.isauth) {
+    route = (
       <Switch>
         <Route exact path="/" component={Burgerbuilder} />
-        <Route exact path="/auth" component={Auth} />
+        <Route exact path="/orders" render={() => <Order />} />
+        <Route exact path="/auth" render={() => <Auths />} />
+        <Route path="/checkout" render={() => <Checkout />} />
+        <Route path="/logout" component={Logout} />
         <Redirect to="/" />
       </Switch>
     );
-    if (this.props.isauth) {
-      route = (
-        <Switch>
-          <Route exact path="/" component={Burgerbuilder} />
-          <Route exact path="/orders" component={Orders} />
-          <Route exact path="/auth" component={Auth} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/logout" component={Logout} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-    return (
-      <div>
-        <Layout>{route}</Layout>
-      </div>
-    );
   }
-}
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>loading............</p>}>{route} </Suspense>
+      </Layout>
+    </div>
+  );
+};
+
 const mapstatetoprops = (state) => {
   return {
     isauth: state.Auth.tokenid !== null,
--- src/index.js
+++ src/index.js
@@ -12,14 +12,11 @@ import authreduecr from "./Store/reducers/auth";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
 import thunk from "redux-thunk";
-import createSagaMiddleware from "react-saga";
-import { sagalogout } from "./Saga/auth";
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null || compose;
-const sagaMiddleware = createSagaMiddleware();
 const rootreducer = combineReducers({
   burgerbuilder: burgerbuilderreducer,
   order: orderreducer,
@@ -27,9 +24,8 @@ const rootreducer = combineReducers({
 });
 const store = createStore(
   rootreducer,
-  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
+  composeEnhancers(applyMiddleware(thunk))
 );
-sagaMiddleware.run(sagalogout);
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
